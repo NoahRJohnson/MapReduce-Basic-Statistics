1937b26a0c98a7febb14c14dfa4f3c4ec80a2996
Noah R. Johnson <noah.johnson13@ncf.edu>
2018-02-17 11:54:12 -0500

    Q1 complete

--- summarizeStates.py
+++ summarizeStates.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 from mrjob.job import MRJob
+import numpy as np
 
 '''
 Calculate the largest, smallest, and average (mean) population for a state. Calculate the
@@ -12,35 +13,58 @@ Name, Abbreviation (Long), 2-Letter Abbreviation, Area (Sq. Miles), Population
 class MRSummarize(MRJob):
 
     def mapper(self, _, line):
-        name, abr, area, pop = line.split()
+        name, abr_long, abr, area, pop = line.split(',')
         
-        yield abr, (pop, area)
+        area = int(area)
+        pop = int(pop)
+        
+        yield "states", (abr, pop, area)
         
     def reducer(self, key, values):
         nValues = 0
         totalPop = 0
         totalArea = 0
         
-        maxPop = 0
-        maxArea = 0
+        maxPop = -1
+        maxPopName = ""
+        maxArea = -1
+        maxAreaName = ""
         
-        minPop = 0
-        minArea = 0
+        minPop = np.inf
+        minPopName = ""
+        minArea = np.inf
+        minAreaName = ""
         
         # this for-loop goes until all mappers finish
-        for pop, area in values:
+        for name, pop, area in values:
             totalPop += pop
             totalArea += area
             nValues += 1
             
-            maxPop = max(maxPop, pop)
-            maxArea = max(maxArea, area)
+            if pop > maxPop:
+                maxPop = pop
+                maxPopName = name
+            if area > maxArea:
+                maxArea = area
+                maxAreaName = name
             
-            minPop = min(minPop, pop)
-            minArea = min(minArea, area)
+            if pop < minPop:
+                minPop = pop
+                minPopName = name
+            if area < minArea:
+                minArea = area
+                minAreaName = name
             
+        avgPop = round(float(totalPop) / nValues, 2)
+        avgArea = round(float(totalArea) / nValues, 2)
+        
+        vals = [str(n) + " (" + s + ")" for n,s in zip([maxPop, minPop, maxArea, minArea], [maxPopName, minPopName, maxAreaName, minAreaName])]
+        vals.insert(2, avgPop)
+        vals.append(avgArea)
+        
         labels = "Largest Pop,Smallest Pop,Average Pop,Largest Area,Smallest Area,Average Area".split(",")
-        d = dict(zip(labels, [maxPop, minPop, round(float(totalPop) / nValues, 2), maxArea, minArea, round(float(totalArea) / nValues, 2)]))
+        d = dict(zip(labels, vals))
+        
         yield key, d
 
 
